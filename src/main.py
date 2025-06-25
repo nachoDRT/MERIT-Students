e40d509e81e7c0a448d1438f6a1ebaea69901f0a
nachoDRT <iderodrigo@comillas.edu>
2025-06-25 11:02:35 +0200

    Fix Bug Managing Keys and Values from Config when Filtering Data

--- src/main.py
+++ src/main.py
@@ -50,12 +50,15 @@ def get_fair_face_subsets(decode=None):
 
     subsets = {}
 
-    for age_id, age_tag in AGE_GROUPS.items():
-        for race_id, race_tag in ORIGINS.items():
-            for gender_id, gender_tag in GENDERS.items():
-                key = f"{race_tag}_{gender_tag}_{age_tag}"
+    for age_tag, age_id in AGE_GROUPS.items():
+        for origin_tag, origin_id in ORIGINS.items():
+            for gender_tag, gender_id in GENDERS.items():
+                key = f"{origin_tag}_{gender_tag}_{age_tag}"
+
+                """ As the authors of this dataset, we prefer to use the term "origin", 
+                but "race" is used in Fair Face dataset. Thus we use it to access that column in the df"""
                 subsets[key] = fair_face_dataset.filter(
-                    lambda ex, a=age_id, r=race_id, g=gender_id: (
+                    lambda ex, a=age_id, r=origin_id, g=gender_id: (
                         ex["age"] == a and ex["gender"] == g and ex["race"] == r
                     ),
                     num_proc=NUM_PROC,
@@ -69,7 +72,7 @@ if __name__ == "__main__":
     merit_df = get_merit_data()
     fair_face_subsets = get_fair_face_subsets()
 
-    east_asian_women_aged_20_29 = fair_face_subsets["east_asian_women_aged_20_29"]
-    latino_men_aged_10_19 = fair_face_subsets["latino_men_aged_10_19"]
-    print(f"Num of young_students_men: {len(east_asian_women_aged_20_29)}")
-    print(f"Num of young_students_women: {len(latino_men_aged_10_19)}")
+    east_asian_women_20_to_29 = fair_face_subsets["east_asian_women_20_to_29"]
+    latino_men_10_to_19 = fair_face_subsets["latino_men_10_to_19"]
+    print(f"Num of young_students_men: {len(east_asian_women_20_to_29)}")
+    print(f"Num of young_students_women: {len(latino_men_10_to_19)}")
