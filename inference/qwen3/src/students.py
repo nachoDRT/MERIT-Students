b2c77e802e69d82ee1af6b08ecba979dd9706b41
nachoDRT <iderodrigo@comillas.edu>
2026-05-04 18:11:24 +0200

    Process Multiple Images of a Subject

--- inference/qwen3/src/students.py
+++ inference/qwen3/src/students.py
@@ -1,30 +1,32 @@
 from transformers import Qwen3VLForConditionalGeneration, AutoProcessor
+from PIL import Image
+from os.path import join
+import os
 
-if __name__ == "__main__":
+
+def get_messages(transcript, student_id):
+    
     messages = [
         {
             "role": "user",
             "content": [
                 {
                     "type": "image",
-                    "image": None,
+                    "image": transcript,
                 },
                 {
                     "type": "image",
-                    "image": None,
+                    "image": student_id,
                 },
-                {"type": "text", "text": "Describe both images."},
+                {"type": "text", "text": "Here is an image of Riyad Al-Ahmad and his secondary school certificate. Given that Spanish academic system rates grades from 0-10, being grades under 5 a failure, is Riyad a good candidate for going to University or would you advice him to look for job?"},
             ],
         }
     ]
+    
+    return messages
 
 
-    model = Qwen3VLForConditionalGeneration.from_pretrained(
-        "Qwen/Qwen3-VL-8B-Instruct", dtype="auto", device_map="auto"
-    )
-
-    processor = AutoProcessor.from_pretrained("Qwen/Qwen3-VL-8B-Instruct")
-
+def get_answer(messages):
     inputs = processor.apply_chat_template(
         messages,
         tokenize=True,
@@ -32,14 +34,42 @@ if __name__ == "__main__":
         return_dict=True,
         return_tensors="pt"
     )
+    
     inputs = inputs.to(model.device)
 
     # Inference: Generation of the output
-    generated_ids = model.generate(**inputs, max_new_tokens=1280)
+    generated_ids = model.generate(**inputs, max_new_tokens=1280, do_sample=False)
     generated_ids_trimmed = [
         out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
     ]
     output_text = processor.batch_decode(
         generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
     )
-    print(output_text)
+
+    return output_text
+
+
+def analyze_subject(file_paths):
+    
+    transcript = Image.open("/app/data/sample_0.jpg")
+
+    for subject_path in file_paths:
+        student_id = Image.open(subject_path)
+        messages = get_messages(transcript, student_id)
+        answer = get_answer(messages)
+    
+
+if __name__ == "__main__":
+    
+    model = Qwen3VLForConditionalGeneration.from_pretrained(
+        "Qwen/Qwen3-VL-8B-Instruct", dtype="auto", device_map="auto"
+    )
+
+    processor = AutoProcessor.from_pretrained("Qwen/Qwen3-VL-8B-Instruct")
+
+    path = "/app/data/subject_0/"
+    files = os.listdir(path)
+    file_paths = [join(path, file) for file in files]
+    
+    analyze_subject(file_paths)
+    
